2cd48cb9e34762c7fb0a25501521ed9730b3d55c
A <amitsoniforbhu@gmail.com>
2017-11-02 21:56:53 +0530

    Update setupcontroller.js

--- controller/setupcontroller.js
+++ controller/setupcontroller.js
@@ -1,14 +1,14 @@
 module.exports = function(server , restify , plugins  , restifyValidator){ 
 
     // use handeler chain is executed after a route has been choosen to service a Request
-    server.use(plugins.acceptParser(server.acceptable));
-    server.use(plugins.bodyParser());
-    server.use(plugins.queryParser());
-    server.use(restifyValidator);
-    server.use(plugins.authorizationParser());
+    server.use(plugins.acceptParser(server.acceptable));                
+    server.use(plugins.bodyParser());                               //  
+    server.use(plugins.queryParser());                              //  
+    server.use(restifyValidator);                                   // Restify Plugin                                   
+    server.use(plugins.authorizationParser());                      // Autherization Plugin
     server.use(function(req,res,next){
          /*
-            ip whitelisting additional  Implementatin 
+            ip whitelisting additional  Implementation  for specific IP 
             
             var IpList = ['111.111.222.111'];
             var Ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
@@ -34,7 +34,7 @@ module.exports = function(server , restify , plugins  , restifyValidator){
     
     server.use(function(req,res,next){
         var apiKeys = {
-            'user1': 'fdfdfdfdfergfgfg'
+            'user1': 'fdfdfdfdfergfgfg'                 // Assuming API key 
         };
         if(typeof(req.authorization.basic) === 'undefined' || !apiKeys[req.authorization.basic.username] || req.authorization.basic.password != apiKeys[req.authorization.basic.username])
         {
@@ -54,7 +54,8 @@ module.exports = function(server , restify , plugins  , restifyValidator){
         // next() does not take any parameter
         // next(false) to stop any processing request
     });
-
+    //Remove this if you want to use throttle 
+    
     // Throttle is use when preventing multiple request Simultneously Only request/rate will accepted
     // server.use(plugins.throttle({
     //     rate:1,            // rate a request can pass
