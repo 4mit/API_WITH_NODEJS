117bc5bef6d73f367fc1e44b6f133e9e8654fb73
A <amitsoniforbhu@gmail.com>
2017-11-02 21:59:09 +0530

    Update UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -3,12 +3,12 @@ var mongoose = require('mongoose');     // Importing mongoose package
 var Schema = mongoose.Schema,           // schema Objects
 ObjectId = Schema.ObjectId;
 
-var UserModel = mongoose.model('user', new Schema({   
-    id        : ObjectId,
+var UserModel = mongoose.model('user', new Schema({         // Creating UserModel
+    id             : ObjectId,                              // We can Add any number of parameter 
     first_name     : String,
     last_name      : String,
-    email_address : String,
+    email_address  : String,
     career         : String 
 }));
 
-module.exports = UserModel;
+module.exports = UserModel;                                 // Exporting UserModel
